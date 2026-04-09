59f3ef0bae6f2f3c12d8605922483769e025a116
EkaterinaAsanova <u-kat@mail.ru>
2026-04-09 06:37:37 +0500

    Initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/CustomViewMatcher.java
@@ -0,0 +1,31 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
+import org.hamcrest.Description;
+
+public class CustomViewMatcher {
+    public static BoundedMatcher<View, RecyclerView> recyclerViewSizeMatcher(final int matcherSize) {
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+
+            private String result;
+
+            @Override
+            public void describeTo(Description description) { // Доп. описание ошибки
+                description.appendText("Request list size: " + matcherSize);
+                description.appendText("Actual list size: ");
+                description.appendText(result);
+            }
+
+            @Override
+            protected boolean matchesSafely(RecyclerView recyclerView) { // Проверка
+                int items = recyclerView.getAdapter().getItemCount();
+                result = "List size: " + items;
+                return matcherSize == items;
+            }
+        };
+    }
+}
---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IdlingResourceTest.java
@@ -0,0 +1,48 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class IdlingResourceTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void registerIdlingResources() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+
+    @Test
+    public void testsOpenGalleryWithViewMatcher() {
+        ViewInteraction menu = onView(isAssignableFrom(AppCompatImageButton.class));
+        menu.check(matches(isDisplayed()));
+        menu.perform(click());
+
+        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
+        gallery.perform(click());
+
+        ViewInteraction recycleView = onView(withId(R.id.recycle_view));
+        recycleView.check(matches(isDisplayed()));
+    }
+}
